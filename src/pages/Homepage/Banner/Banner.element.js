97abaa7e3e1c596276bf5439ea0bdf148a925504
PTBYSR <paul.emechebe@gmail.com>
2022-05-29 20:46:03 +0100

    sone-styling-banner

--- src/pages/Homepage/Banner/Banner.element.js
+++ src/pages/Homepage/Banner/Banner.element.js
@@ -6,13 +6,14 @@ export const BannerSection = styled.section`
     padding: 0 10vw;
     position: relative;
     overflow: hidden;
+    gap: 0rem;
 `
 export const Ellipse = styled.img`
     border: 2px dashed blue;
     position: absolute;
     left: 109px;
     bottom: -30px;
-    width: 40vw;
+    width: 41vw;
 `
 export const Girls = styled.img`
     border: 2px dashed blue;
@@ -22,7 +23,42 @@ export const Girls = styled.img`
 `
 export const ImageSection = styled.div`
     border: 1px solid pink;
+    width: 50%;
 `
 export const HeadingSection = styled.div`
     border: 1px solid green;
+    width: 50%;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 5vw 0vw 5vw 6.5vw;
+    gap: 0rem;
+`
+export const HeadingWrapper = styled.div`
+    border: 1px solid green;
+`
+export const Share = styled.div`
+    border: 1px solid green;
+    position:relative;
+`
+export const Slash = styled.img`
+    width: 14vw;
+    position: absolute;
+    left: -9px;
+    bottom: -6px;
+`
+export const TextBody = styled.div`
+    border: 1px solid green;
+    height: 10vw
+`
+export const ShareBtn = styled.div`
+    font-size: .97vw;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+`
+export const Customer = styled.p`
+    display: inline-block;
+    padding: 5px 6px;
+    border: 1px solid rgba(255, 255, 255, 0.4);
+    border-radius: 4px;
+    margin-top: 5px;
 `
--- src/pages/Homepage/Banner/Banner.js
+++ src/pages/Homepage/Banner/Banner.js
@@ -4,20 +4,38 @@ import {
   ImageSection,
   HeadingSection,
   Ellipse,
-  Girls
+  Girls,
+  HeadingWrapper,
+  Share,
+  ShareBtn,
+  TextBody,
+  Slash,
+  Customer,
 } from "./Banner.element"
 import ellipse from './Ellipse.png'
 import blackGirls from './black-girls.png'
+import slash from './slash.svg'
+
 
 const Banner = () => {
   return (
-    <BannerSection className='bg-gray'>
+    <BannerSection className='bg-gray flex'>
     <ImageSection>
       <Ellipse src={ellipse}></Ellipse>
       <Girls src={blackGirls}></Girls>
     </ImageSection>
-    <HeadingSection>
-
+    <HeadingSection className='text-white text-white ff-inter flex'>
+      <HeadingWrapper>
+        <h1 className='fw-700 ' style={{fontSize: "2vw"}}>Tolu & Joy's Experience</h1>
+        <Customer className="fw-500" style={{fontSize: ".8vw"}}>CUSTOMER</Customer>
+      </HeadingWrapper>
+      <TextBody>
+        <p className="fw-400 fs-18" style={{fontSize:"1.2vw"}}>I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back! I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back!</p>
+      </TextBody>
+      <Share>
+       <ShareBtn className="fw-500">SHARE YOUR STORY</ShareBtn>
+       <Slash src={slash} alt=""></Slash>
+      </Share>
     </HeadingSection>
     </BannerSection>
   )
